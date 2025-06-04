958ae4b563d1a0b9362ed168d8f2d157620a5d99
kithusibrian <briaokm@gmail.com>
2025-06-04 16:38:15 +0300

    Minor Backend Changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,9 +4,12 @@ import { Heart, CarFront, Layout, ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { checkUser } from "@/lib/checkUser";
 
 const Header = async ({ isAdminPage = false }) => {
-  const isAdmin = false;
+
+  const user = await checkUser();
+  const isAdmin = user?.role==="ADMIN";
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="mx-auto px-4 py-4 flex items-center justify-between">
